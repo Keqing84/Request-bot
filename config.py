557c89e8ac41d88c8a04d8aeb8edb6ac97f75613
Sensei Keqing <k.izuka@yandex.com>
2022-03-04 10:10:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-from os import environ as env
+import dotenv
+from os import environ as env, path as ospath
+
+if ospath.exists(".env"):
+   dotenv.load_dotenv()
 
 class Config:
     API_ID = int(env.get('API_ID'))
@@ -6,3 +10,4 @@ class Config:
     BOT_TOKEN = env.get('BOT_TOKEN')
     CHANNEL = int(env.get('CHANNEL'))
     AUTH_CHATS = env.get('AUTH_CHATS')
+    IMG = env.get('IMG', "https://imgwhale.xyz/5taf21l07yc75m")

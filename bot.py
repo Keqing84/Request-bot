1756bac731ecf3f595f090147e66555de30438f1
Sensei Keqing <k.izuka@yandex.com>
2023-02-11 23:05:32 +0530

    Update bot.py
    Big Changes

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO,
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 app = Client(
-         ":memory:", 
+         "Req Bot", 
          api_id = Config.API_ID, 
          api_hash = Config.API_HASH, 
          bot_token = Config.BOT_TOKEN
@@ -31,28 +31,27 @@ else:
 
 # Start Message
 @app.on_message(filters.private & filters.command("start", prefixes=prefixes))
-async def start_msg(message: Message, bot: Client):
-   mention = message.from_user.mention(style="md")
+async def start_msg(_, msg: Message):
+   mention = msg.from_user.mention(style="md")
    text = f'Hello {mention},\nI am A Requesting Bot, Here You Can Me Request With Cmd `/request query` That Is To Be Fulfilled by The Admin(s)/Owner.'
-   await bot.send_photo(
-             chat_id=message.chat.id, 
-             photo=Config.IMG, 
+   await msg.reply_photo(
+             Config.IMG, 
              caption=text,
              parse_mode="md",
-             protect_content=True
+             quote=True
      )
 
 # Request Cmd
 @app.on_message(~filters.edited & filters.command(["request", "req"], prefixes=prefixes))
-async def request_msg(message: Message, bot: Client):
-   id = message.chat.id
+async def request_msg(_, msg: Message):
+   id = msg.chat.id
    if str(id) in AUTH_CHATS:
      pass
    else:
      return
-   text = message.text
+   text = msg.text
    if not " " in text:
-     return await message.reply_text("Send A Query Also with The Cmd in A Single Message.")
+     return await msg.reply_text("Send A Query Also with The Cmd in A Single Message.")
    else:
      pass
    reqtext = text.split(" ", 1)[-1]
@@ -68,7 +67,7 @@ async def request_msg(message: Message, bot: Client):
              ]
           ]
         )
-   K = await message.reply("`.....`", quote=True)
+   K = await msg.reply("`.....`", quote=True)
    try:
       rtext = f"A New Request Has Been Made\n\nRequest: {reqtext}\n\nFrom: {mention} | `{user.id}`"
       await bot.send_message(chat_id=Config.CHANNEL,
@@ -80,7 +79,7 @@ async def request_msg(message: Message, bot: Client):
              )
    except Exception as e:
       LOGGER.info(e)
-      return await k.edit_text("**Error:**\n`{str(e)}`")
+      return await k.edit_text("**Error:**\n\n`{str(e)}`")
 
    try:
      await K.edit_text("My Part Is Done. Now U Just Have To Wait For The Admin/Owner To Proved or Disapprove It.")
@@ -92,14 +91,14 @@ async def request_msg(message: Message, bot: Client):
 
 # CallBack Query
 @app.on_callback_query()
-async def py_data(bot: Client, query: CallbackQuery):
+async def py_data(_, query: CallbackQuery):
    if query.data == "donee":
      msg = "~~" + query.message + "~~" + "\n\nCompleted✅"
      try:
        await query.message.edit_text(msg)
      except Exception as e:
        LOGGER.info(e)
-       await bot.send_message(Config.Channel, text=f"**Error:**\n`{str(e)}`", reply_to_message_id=query.id)
+       await app.send_message(Config.Channel, text=f"**Error:**\n`{str(e)}`", reply_to_message_id=query.id)
 
    # Give Up Callback
    if query.data == "give_up":
@@ -108,7 +107,7 @@ async def py_data(bot: Client, query: CallbackQuery):
        await query.message.edit_text(msg)
      except Exception as e:
        LOGGER.info(e)
-       await bot.send_message(Config.Channel, text=f"**Error:**\n`{str(e)}`", reply_to_message_id=query.id)
+       await app.send_message(Config.Channel, text=f"**Error:**\n`{str(e)}`", reply_to_message_id=query.id)
    else:
      pass
 
